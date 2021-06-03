82f23028f67d92104379eddb0784c1e60a0bf18d
fromcsl <84374335+fromcsl@users.noreply.github.com>
2021-06-04 00:23:40 +0800

    Add files via upload

---
+++ start.h
@@ -0,0 +1,23 @@
+#ifndef START_H
+#define START_H
+#include<QPainter>
+#include <QWidget>
+#include<second.h>
+#include<QTimer>
+#include<QDateTime>
+#include<QLabel>
+class Start : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Start(QWidget *parent = 0);
+    ~Start();
+    Second *sec=NULL;
+    void paintEvent(QPaintEvent *);
+    QLabel label;
+public slots:
+
+};
+
+#endif // START_H
